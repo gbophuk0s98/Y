ef31a61ffd93cdf8e960add7560f007981534e84
Pavel, Surta <surta.iit1@gmail.com>
2024-03-06 21:58:44 +0300

    initial structure [to be changed]

---
+++ src/test/java/com/example/y/YApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.y;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
